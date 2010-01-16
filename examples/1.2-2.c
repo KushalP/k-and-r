ea2292964ad2329e619a55ec4cd84be184cab2b1
Kushal Pisavadia <kushi.p@gmail.com>
2010-01-16 13:26:59 +0000

    converted example 1-2.1.c to use floats for accuracy

---
+++ examples/1.2-2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* print Fahrenheit-Celsius table
+	for fahr = 0, 20, ..., 300 */
+
+main()
+{
+	float fahr, celsius;
+	float lower, upper, step;
+	
+	lower = 0;		//Lower limit of temperature scale
+	upper = 300;	//Upper limit
+	step = 20;		//Step size
+	
+	fahr = lower;
+	
+	while (fahr <= upper)
+	{
+		celsius = 5 * (fahr - 32) / 9;
+		printf("%3.0f\t%6.1f\n", fahr, celsius);
+		fahr = fahr + step;
+	}
+}
