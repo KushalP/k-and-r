977de6e83184f7c4c9681cc55a2cb927f54e739d
Kushal Pisavadia <kushi.p@gmail.com>
2010-01-16 22:02:50 +0000

    started work on functions, converted temperature list to use a function

---
+++ exercises/1-15.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* Rewrite the temperature conversion program of 
+	Section␣1.2 to use a function for conversion. */
+
+float to_celsius(int fahr);
+
+main()
+{
+	int i = 0;
+	
+	for (i = 0; i <= 300; i += 20)
+		printf("%5d | %5.2f\n", i, to_celsius(i));
+	
+	return 0;
+}
+
+float to_celsius(int fahr)
+{
+	return (5.0/9.0) * (fahr - 32.0);
+}
