1af57e7d789baf18b6b11a235060bc042f43f32c
Kushal Pisavadia <kushi.p@gmail.com>
2010-01-16 13:29:10 +0000

    updated to use float constants instead of integers to improve readability

--- examples/1.2-2.c
+++ examples/1.2-2.c
@@ -16,7 +16,7 @@ main()
 	
 	while (fahr <= upper)
 	{
-		celsius = 5 * (fahr - 32) / 9;
+		celsius = (5.0/9.0) * (fahr - 32.0);
 		printf("%3.0f\t%6.1f\n", fahr, celsius);
 		fahr = fahr + step;
 	}
