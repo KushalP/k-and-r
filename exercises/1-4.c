ec7416ca17a500ae43a3c7de1feb9d5368f644cc
Kushal Pisavadia <kushi.p@gmail.com>
2010-01-16 15:37:41 +0000

    switched temperature table to be C -> F rather than F -> C

---
+++ exercises/1-4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*
+	print Celsius-Fahrenheit table
+*/
+main()
+{
+	float fahr, celsius;
+	float lower, upper, step;
+	
+	lower = 0;		//Lower limit of temperature scale
+	upper = 100;	//Upper limit
+	step = 1;		//Step size
+	
+	fahr = lower;
+	
+	printf("%10s\t%12s\n", "Celsius", "Fahrenheit");
+	
+	while (celsius <= upper)
+	{
+		fahr = (9.0/5.0) * (celsius+32);
+		printf("%10.0f\t%11.1f\n", celsius, fahr);
+		celsius = celsius + step;
+	}
+}
