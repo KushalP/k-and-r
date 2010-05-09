3fe1f3f9edf9ffa9b4f506640626759bcfd3cd6c
Kushal Pisavadia <kushi.p@gmail.com>
2010-05-09 15:01:18 +0100

    Copied over the exercise outline. Looks like it's a regex parser of some kind that will take some thought to complete. The leading dash taken literally could be an interesting problem in itself. Perhaps I could utilise the previous switch statement exercise for this?

---
+++ Chapter-3/exercises/3-3.c
@@ -0,0 +1,10 @@
+// TODO:
+// 	Write a function expand(s1, s2) that expands
+// 	shorthand notations like 'a-z' in the string
+// 	s1 into the equivalent complete list acb..xyz
+// 	in s2.
+//
+// 	Allow for letters of either case and digits, 
+// 	and be prepared to handle cases like a-b-c
+//	and a-z0-9 and -a-z. Arrange that a leading
+//	or trailing '-' is taken literally.
