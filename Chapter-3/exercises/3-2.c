b6f9c9c442cb56e2290a440f4a8d4f294fc8b42b
Kushal Pisavadia <kushi.p@gmail.com>
2010-05-09 14:47:37 +0100

    Completed the switch statement exericse, which involved a lot of back and forth looking through the escape character table! Coming back in the other direction was slightly harder as it needed a bit of extra thought about what sequence could be checked against.

--- Chapter-3/exercises/3-2.c
+++ Chapter-3/exercises/3-2.c
@@ -5,7 +5,7 @@ void unescape(char* s, char* t);
 
 int main()
 {
-	char text1[50] = "\aHello, Neo.\n\tWelcome to\b the \"real\" 'world'!\n";
+	char text1[50] = "\a\tHello, Neo.\n\tWelcome to\b the \"real\" 'world'!";
 	char text2[51];
 
 	printf("Original string:\n%s\n", text1);
@@ -19,10 +19,140 @@ int main()
 	return 0;
 }
 
+/**
+ * Copies string t to string s, converting special
+ * characters into their relevant escape sequences.
+ *
+ * The complete set of escapse founds in K&R Chapter
+ * 2 is used, with the exception of:
+ *
+ * \? \' \ooo \xhh
+ *
+ * as these can be typed directly into the source code,
+ * witout using the escape sequences themselves, and 
+ * translating them is WEIRD.
+ */
 void escape(char* s, char* t)
 {
+	int i = 0, j = 0;
+
+	while (t[i])
+	{
+		switch(t[i])
+		{
+			case '\n':
+				s[j++] = '\\';
+				s[j] = 'n';
+				break;
+			case '\t':
+				s[j++] = '\\';
+				s[j] = 't';
+				break;
+			case '\a':
+				s[j++] = '\\';
+				s[j] = 'a';
+				break;
+			case '\b':
+				s[j++] = '\\';
+				s[j] = 'b';
+				break;
+			case '\f':
+				s[j++] = '\\';
+				s[j] = 'f';
+				break;
+			case '\r':
+				s[j++] = '\\';
+				s[j] = 'r';
+				break;
+			case '\v':
+				s[j++] = '\\';
+				s[j] = 'v';
+				break;
+			case '\\':
+				s[j++] = '\\';
+				s[j] = '\\';
+				break;
+			case '\"':
+				s[j++] = '\\';
+				s[j] = '\"';
+				break;
+			default:
+				// This is not a special character, 
+				// so just copy it
+				s[j] = t[i];
+				break;
+		}
+
+		i++;
+		j++;
+	}
+
+	s[j] = t[i]; // Don't forget the null character!
 }
 
+/**
+ * Copies string t to strin s, converting escape
+ * characters into their relevant special characters.
+ * 
+ * See the above comment on escape() for which escape
+ * sequences are translated.
+ */
 void unescape(char* s, char* t)
 {
+	int i = 0, j = 0;
+
+	while (t[i])
+	{
+		switch (t[i])
+		{
+			case '\\':
+				// We've found an escape sequence,
+				// so translate it
+				switch (t[++i])
+				{
+					case 'n':
+						s[j] = '\n';
+						break;
+					case 't':
+						s[j] = '\t';
+						break;
+					case 'a':
+						s[j] = '\a';
+						break;
+					case 'b':
+						s[j] = '\b';
+						break;
+					case 'f':
+						s[j] = '\f';
+						break;
+					case 'r':
+						s[j] = '\r';
+						break;
+					case 'v':
+						s[j] = '\v';
+						break;
+					case '\\':
+						s[j] = '\\';
+						break;
+					case '\"':
+						s[j] = '\"';
+						break;
+					default:
+						// We don't translate this escape 
+						// sequence, so just copy it
+						s[j++] = '\\';
+						s[j] = t[i];
+				}
+				break;
+			default:
+				// Not an escape sequence, 
+				// so just copy it
+				s[j] = t[i];
+		}
+		
+		i++;
+		j++;
+	}
+
+	s[j] = t[i]; // Don't forget the null character!
 }
