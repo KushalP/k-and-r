26cec9e9caed58e08a24fd8169be25f0995e5805
Albrechtcolia <albrechtcolia@gmail.com>
2022-10-28 01:26:23 -0300

    Fixing Celsius-Fahrenheit converter

--- Chapter 1 - A Tutorial Introduction/exercises/1-4.c
+++ Chapter 1 - A Tutorial Introduction/exercises/1-4.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
 /*
-	print Celsius-Fahrenheit table
-*/
-int main()
+ * Printf Celsius-Fahrenheit table.
+ */
+
+int main (void)
 {
 	float fahr, celsius;
 	float lower, upper, step;
 	
-	lower = 0;		//Lower limit of temperature scale
-	upper = 100;	//Upper limit
-	step = 1;		//Step size
-	
+	lower = 0;      // Lower limit of temperature scale.
+	upper = 100;    // Upper limit.
+	step  = 1;      // Step size.
+
+	printf("Celsius\t\tFahrenheit\n");
+
 	fahr = lower;
-	
-	printf("%10s\t%12s\n", "Celsius", "Fahrenheit");
-	
 	while (celsius <= upper)
 	{
-		fahr = (9.0/5.0) * (celsius+32);
-		printf("%10.0f\t%11.1f\n", celsius, fahr);
-		celsius = celsius + step;
+		fahr = ((9.0/5.0) * celsius) + 32;
+		printf("%3.0f\t\t%6.1f\n", celsius, fahr);
+		celsius += step;
 	}
 
 	return 0;
